1d07e3f555b54883c6b42f4bf943c1a399a24c97
stolomeo <tolomeosebastian@gmail.com>
2024-05-01 19:35:01 -0400

    refactor: Remove repetition

--- client/src/utils/getBalanceSheetElements.ts
+++ client/src/utils/getBalanceSheetElements.ts
@@ -1,51 +1,28 @@
 import { CompanyBalanceSheet } from "../types";
 import { formatLargeMonetaryNumber } from "./formatting";
 
+type BalanceSheetElements = {
+  key: keyof CompanyBalanceSheet;
+  label: string;
+};
+
 export const getBalanceSheetElements = (balanceSheet: CompanyBalanceSheet) => {
-  return [
-    {
-      label: "Total Assets",
-      total: formatLargeMonetaryNumber(balanceSheet.totalAssets),
-    },
-    {
-      label: "Current Assets",
-      total: formatLargeMonetaryNumber(balanceSheet.totalCurrentAssets),
-    },
-    {
-      label: "Total Cash",
-      total: formatLargeMonetaryNumber(balanceSheet.cashAndCashEquivalents),
-    },
-    {
-      label: "Property & Equipment",
-      total: formatLargeMonetaryNumber(balanceSheet.propertyPlantEquipmentNet),
-    },
-    {
-      label: "Intangible Assets",
-      total: formatLargeMonetaryNumber(balanceSheet.intangibleAssets),
-    },
-    {
-      label: "Long Term Debt",
-      total: formatLargeMonetaryNumber(balanceSheet.longTermDebt),
-    },
-    {
-      label: "Total Debt",
-      total: formatLargeMonetaryNumber(balanceSheet.totalDebt),
-    },
-    {
-      label: "Total Liabilities",
-      total: formatLargeMonetaryNumber(balanceSheet.totalLiabilities),
-    },
-    {
-      label: "Current Liabilities",
-      total: formatLargeMonetaryNumber(balanceSheet.totalCurrentLiabilities),
-    },
-    {
-      label: "Stakeholder's Equity",
-      total: formatLargeMonetaryNumber(balanceSheet.totalStockholdersEquity),
-    },
-    {
-      label: "Retained Earnings",
-      total: formatLargeMonetaryNumber(balanceSheet.retainedEarnings),
-    },
+  const elements: BalanceSheetElements[] = [
+    { key: "totalAssets", label: "Total Assets" },
+    { key: "totalCurrentAssets", label: "Current Assets" },
+    { key: "cashAndCashEquivalents", label: "Total Cash" },
+    { key: "propertyPlantEquipmentNet", label: "Property & Equipment" },
+    { key: "intangibleAssets", label: "Intangible Assets" },
+    { key: "longTermDebt", label: "Long Term Debt" },
+    { key: "totalDebt", label: "Total Debt" },
+    { key: "totalLiabilities", label: "Total Liabilities" },
+    { key: "totalCurrentLiabilities", label: "Current Liabilities" },
+    { key: "totalStockholdersEquity", label: "Stakeholder’s Equity" },
+    { key: "retainedEarnings", label: "Retained Earnings" },
   ];
+
+  return elements.map(({ key, label }) => ({
+    label,
+    total: formatLargeMonetaryNumber(balanceSheet[key]),
+  }));
 };
